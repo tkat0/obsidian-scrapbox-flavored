d1228d77cf795063763ba60715d876766976671f
tkat0 <tkato.dev@gmail.com>
2022-10-08 08:08:32 +0900

    chore: fmt

--- packages/grid-view/src/view/StatusBar.svelte
+++ packages/grid-view/src/view/StatusBar.svelte
@@ -2,10 +2,10 @@
   export let total: number;
 </script>
 
-<div class="absolute bottom-0 right-0 w-full flex">
+<div class="absolute bottom-0 right-0 flex w-full">
   <div class="grow" />
   <div
-    class="border-t-[1px] border-l-[1px] border-black border-text-accent-hover py-[2px] px-[4px] bg-background-primary"
+    class="border-text-accent-hover bg-background-primary border-t-[1px] border-l-[1px] border-black py-[2px] px-[4px]"
   >
     {total} pages
   </div>
